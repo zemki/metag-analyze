51d20c0f2d1f0a794f345533ca5f6dad4938c815
Alessandro Belli <alessandrobelli90@gmail.com>
2019-09-10 10:19:36 +0200

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,14 +23,14 @@ class UsersTableSeeder extends Seeder
     		'api_token' => hash('sha256',str_random(60)),
     		'created_at' => $faker->dateTime($max = 'now', $timezone = "Europe/Berlin"),
     		'updated_at' => $faker->dateTime($max = 'now', $timezone = "Europe/Berlin")
-    	],
-    	[
-    		'email' => "fhohmann@uni-bremen.de",
-    		'password' => bcrypt('q1w2e3r4t5'),
-    		'api_token' =>  hash('sha256',str_random(60)),
-    		'created_at' => $faker->dateTime($max = 'now', $timezone = "Europe/Berlin"),
-    		'updated_at' => $faker->dateTime($max = 'now', $timezone = "Europe/Berlin")
     	]);
+    	DB::table('users')->insert([
+            'email' => "fhohmann@uni-bremen.de",
+            'password' => bcrypt('q1w2e3r4t5'),
+            'api_token' =>  hash('sha256',str_random(60)),
+            'created_at' => $faker->dateTime($max = 'now', $timezone = "Europe/Berlin"),
+            'updated_at' => $faker->dateTime($max = 'now', $timezone = "Europe/Berlin")
+        ]);
 
     	$this->command->info('Admin user seeded');
 
