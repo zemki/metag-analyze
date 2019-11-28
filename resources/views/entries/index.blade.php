eab0b1239fe5e2d0473effeeb15bac565a938dd9
Alessandro Belli <alessandrobelli90@gmail.com>
2019-11-28 10:17:00 +0100

    Graph Style Optimization

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -14,26 +14,6 @@
 
 @endsection
 @section('pagespecificcss')
-    @media print{
-    @page {size: landscape}
-    .no-print, .no-print *
-    {
-    display: none !important;
-    }
-    body {
-    margin: 0;
-    background-color: #fff;
-    padding: 0;
-    }
-    .chart-left{
-    width:100%;
-    height:100%;
-    page-break-after:always;
-    margin-left: -150px;
-    }
-    .title-chart{
-    margin-left: -150px;
-    width:100%;
-    }
-    }
+
+
 @endsection
