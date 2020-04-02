e528d7be8c11c001d895819f4e3e57cfe2c96033
ZeMKI <belli@uni-bremen.de>
2020-04-02 15:06:32 +0200

    Bugfix on chart
    * now the date/time on the chart is correct.
    * layout bugfix.
    * tailwind upgrade.

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,14 +1,14 @@
 @if($breadcrumb)
     <div class="sm:hidden md:hidden lg:flex xl:flex">
-        <nav class="flex flex-wrap list-reset pt-3 pb-3 py-4 px-4 mb-4 bg-grey-light rounded has-succeeds-separator is-small" aria-label="breadcrumbs">
-            <ul>
-                <li><a href="#">Metag</a></li>
+        <nav class="flex flex-wrap list-reset pt-3 pb-3 py-4 px-4 mb-4 bg-gray-100 rounded has-succeeds-separator is-small" aria-label="breadcrumbs">
+
+                <div class="inline"><a href="#">Metag ></a></div>
 
                 @foreach($breadcrumb as $url => $title)
-                    <li @if($url==='#')    class="is-active" @endif><a href="{{ $url }}">{{ $title }}</a></li>
+                    <div @if($url==='#')    class="inline" @endif><a href="{{ $url }}">&nbsp; {{ $title }} ></a></div>
                 @endforeach
 
-            </ul>
+
         </nav>
     </div>
 @endif
