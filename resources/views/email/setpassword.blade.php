4edb50ce7520fb15469fd1aa233c0f153e3f418f
ZeMKI <belli@uni-bremen.de>
2020-10-26 10:55:56 +0100

    Invites optimization
    * verification email now displays the expiration time.
    * middleware optimization.
    * new user property -> isInvited.

--- resources/views/email/setpassword.blade.php
+++ resources/views/email/setpassword.blade.php
@@ -7,6 +7,7 @@
     @component('mail::button', ['url' => url('password/set')."?token=".($user->password_token ? $user->password_token : ''), 'color' => 'success'])
         {{__('Set Metag Password')}}
     @endcomponent
+    {{__('This link will expire in 60 minutes.')}}
     <br>
     {{__('Thanks,
     Metag Team')}}
