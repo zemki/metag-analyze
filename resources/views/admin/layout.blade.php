80c9a961c3f2a4dd5fefda8bb41ab8b7511a68ff
Alessandro Belli <alessandrobelli90@gmail.com>
2020-11-17 15:42:23 +0100

    Update layout.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -27,14 +27,6 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                                 @csrf
                             </form>
-                            <svg class="pl-2 mb-2 font-medium leading-tight text-3xl fill-current text-gray-100" version="1.1"
-                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129"
-                                 enable-background="new 0 0 129 129">
-                                <g>
-                                    <path
-                                        d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z"/>
-                                </g>
-                            </svg>
                         </button>
                         <div id="dropdownLogout"
                              class="bg-gray-900 rounded shadow-md absolute mt-8 top-0 right-0 min-w-full overflow-auto z-30 logoutdropdown-content hidden"
@@ -58,7 +50,7 @@
                     <div class="block lg:hidden pr-4">
                         <button id="nav-toggle"
                                 class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-100 hover:border-teal-500 appearance-none focus:outline-none">
-                            <svg class="fill-current mb-2 font-medium leading-tight text-2xl w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <svg class="fill-current mb-2 font-medium leading-tight text-2xl w-3 h4- w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <title>
                                     Menu</title>
                                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
