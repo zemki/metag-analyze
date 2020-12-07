90eb71a7c6313d5196bb7fee7d320c62378e7a3e
Alessandro Belli <belli@uni-bremen.de>
2020-12-07 16:45:36 +0100

    Update setpassword.blade.php

--- resources/views/email/setpassword.blade.php
+++ resources/views/email/setpassword.blade.php
@@ -7,7 +7,6 @@
     @component('mail::button', ['url' => url('password/set')."?token=".($user->password_token ? $user->password_token : '')."&email=".$user->email, 'color' => 'success'])
         {{__('Set Metag Password')}}
     @endcomponent
-    {{__('This link will expire in 60 minutes.')}}
     <br>
     <br>
     <br>
