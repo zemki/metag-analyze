c8989b90fdf0f0090d48ed74457e19bfe436ffc6
Alessandro Belli <belli@uni-bremen.de>
2021-01-27 12:57:13 +0100

    Update ProjectCasesController.php
    * bugfix permission on haribograph

--- app/Http/Controllers/ProjectCasesController.php
+++ app/Http/Controllers/ProjectCasesController.php
@@ -27,7 +27,6 @@ class ProjectCasesController extends Controller
      */
     public function distinctshow(Project $project, Cases $case)
     {
-
         if (auth()->user()->notOwnerNorInvited($project) && !auth()->user()->isAdmin())
         {
             abort(403);
@@ -57,6 +56,11 @@ class ProjectCasesController extends Controller
 
     public function hariboshow(Project $project, Cases $case)
     {
+  
+        if (auth()->user()->notOwnerNorInvited($project) && !auth()->user()->isAdmin())
+        {
+            abort(403);
+        }
         $tempArray = Entry::where('case_id', '=', $case->id)->with('media')->get()->toArray();
         $data['entries'] = [];
         $data['availableInputs'] = $project->getAnswersInputs();
