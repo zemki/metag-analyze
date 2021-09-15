53f3facf31a1dff2a4269a1c073ce510308e1b6a
Alessandro Belli <belli@uni-bremen.de>
2021-09-15 11:33:43 +0200

    Debug push notifications

--- app/Console/Commands/NotificationChecker.php
+++ app/Console/Commands/NotificationChecker.php
@@ -3,31 +3,26 @@
 namespace App\Console\Commands;
 
 use App\Cases;
-use App\Notifications\researcherNotificationToUser;
-use App\User;
 use DB;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Builder;
+use Spatie\WebhookServer\WebhookCall;
 
 class NotificationChecker extends Command
 {
     /**
      * The name and signature of the console command.
-     *
      * @var string
      */
     protected $signature = 'notifications:check';
-
     /**
      * The console command description.
-     *
      * @var string
      */
     protected $description = 'This command check the notification table for recurring notifications and send them.';
 
     /**
      * Create a new command instance.
-     *
      * @return void
      */
     public function __construct()
@@ -37,58 +32,65 @@ class NotificationChecker extends Command
 
     /**
      * Execute the console command.
-     *
      * @return int
      */
     public function handle()
     {
-        $notifications = DB::select('SELECT *  FROM notifications WHERE data NOT LIKE ? and data LIKE ?', ['%"planning":false%','%planning%']);
+        $notifications = DB::select('', SELECT * FROM notifications WHERE data NOT LIKE ? and data LIKE ? ['%"planning":false%', '%planning%']);
         $notificationSent = 0;
         foreach ($notifications as $notification)
         {
             $notification->data = json_decode($notification->data);
-            $case = Cases::where('id',$notification->data->case)->first();
-
+            $case = Cases::where('id', $notification->data->case)->first();
             $caseClosed = ($case->lastDay() !== "" && strtotime($case->lastDay()) < strtotime('now'));
-            if($caseClosed){
-                $this->error("Notification NOT sent to ".$case->user->email." because last day was ".$case->lastDay());
+            if ($caseClosed)
+            {
+                $this->error("Notification NOT sent to " . $case->user->email . " because last day was " . $case->lastDay());
                 // delete planned notification not valid anymore
                 DB::table('notifications')->where('id', '=', $notification->id)->delete();
                 continue;
             }
-
             $timeIsSameAsInDatabase = date('H:i', strtotime('now')) === date('H:i', strtotime(explode('at ', $notification->data->planning)[1]));
-            if($timeIsSameAsInDatabase)
+            if ($timeIsSameAsInDatabase)
             {
-                if((str_contains(explode('at ', $notification->data->planning)[0],"Every day"))){
+                if ((str_contains(explode('at ', $notification->data->planning)[0], "Every day")))
+                {
                     $differenceBetweenLastNotification = 86400;
-                }elseif ((str_contains(explode('at ', $notification->data->planning)[0],"Every two days"))){
+                } elseif ((str_contains(explode('at ', $notification->data->planning)[0], "Every two days")))
+                {
                     $differenceBetweenLastNotification = 86400 * 2;
-                }elseif ((str_contains(explode('at ', $notification->data->planning)[0],"Every three days"))){
+                } elseif ((str_contains(explode('at ', $notification->data->planning)[0], "Every three days")))
+                {
                     $differenceBetweenLastNotification = 86400 * 3;
-                }else{
-                    $this->error("Something went wrong while parsing the frequency of Notification ".$notification->id);
+                } else
+                {
+                    $this->error("Something went wrong while parsing the frequency of Notification " . $notification->id);
                     return 0;
                 }
-
-
                 $timefromdatabase = strtotime($case->user->profile->last_notification_at);
-
                 $dif = time() - $timefromdatabase;
                 $enoughTimeHasPassed = $dif >= $differenceBetweenLastNotification;
-                if($enoughTimeHasPassed)
+                if ($enoughTimeHasPassed)
                 {
                     $this->sendNotification($case, $notification, $notificationSent);
-                }else{
-                    $this->info("notification already sent last ".($timefromdatabase/60/60)."h");
+                } else
+                {
+                    $this->info("notification already sent last " . ($timefromdatabase / 60 / 60) . "h");
                 }
-
-
             }
         }
 
-        if(count($notifications) === 0) $this->warn("0 planned notifications in the database");
-        else $this->info($notificationSent." notification(s) sent.");
+        if (count($notifications) === 0) $this->warn("0 planned notifications in the database");
+        else $this->info($notificationSent . " notification(s) sent.");
+
+        if(config('utilities.notificationcommandtochat') === 1){
+            WebhookCall::create()
+            ->url('https://chat.zemki.uni-bremen.de/hooks/pggPQhGehrPiRSb2S/3xJ2bPWfYk2pqBBhtGGkgb3Q2JMGvH4DKaPdTANSTdZCtfxk')
+            ->payload(['text' => 'Command executed and  ' . $notificationSent . ' Notifications sent. "'])
+            ->useSecret('pggPQhGehrPiRSb2S/3xJ2bPWfYk2pqBBhtGGkgb3Q2JMGvH4DKaPdTANSTdZCtfxk')
+            ->dispatch();
+        }
+
 
         return 0;
     }
@@ -103,7 +105,11 @@ class NotificationChecker extends Command
         $user = $case->user;
         $user->profile->last_notification_at = date("Y-m-d H:i:s");
         $user->profile->save();
-        $user->notify(new researcherNotificationToUser(['title' => $notification->data->title, 'message' => $notification->data->message, 'case' => $case]));
+        $user->notify(new researcherNotif            WebhookCall::create()
+        ->url('https://chat.zemki.uni-bremen.de/hooks/pggPQhGehrPiRSb2S/3xJ2bPWfYk2pqBBhtGGkgb3Q2JMGvH4DKaPdTANSTdZCtfxk')
+        ->payload(['text' => 'Sent a Notification to ' . $this->cases['user']['email'] . ' for the case "' . $this->cases['name'] . '". Poor human being!'])
+        ->useSecret('pggPQhGehrPiRSb2S/3xJ2bPWfYk2pqBBhtGGkgb3Q2JMGvH4DKaPdTANSTdZCtfxk')
+        ->dispatch();icationToUser(['title' => $notification->data->title, 'message' => $notification->data->message, 'case' => $case]));
         $this->info("Notification sent to " . $user->email);
         $notificationSent++;
     }
