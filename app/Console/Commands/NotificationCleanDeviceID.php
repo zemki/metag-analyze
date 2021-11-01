d88c7b5882ff3a7cac6eea94ec44d5a6da1bc021
Alessandro Belli <belli@uni-bremen.de>
2021-11-01 13:53:23 +0100

    Admin area + Notification improvement
    * now we cleanup the deviceid of users that belongs to expired cases
    * improved admin area

---
+++ app/Console/Commands/NotificationCleanDeviceID.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App;
+use App\User;
+use Illuminate\Console\Command;
+use Spatie\WebhookServer\WebhookCall;
+
+class NotificationCleanDeviceID extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'notification:cleanID';
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Remove device ID for users that are not in active cases';
+
+    /**
+     * Create a new command instance.
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * @return int
+     */
+    public function handle()
+    {
+        $usersCleaned = 0;
+        foreach (User::all() as $user)
+        {
+            if ($user->latestCase && $user->latestCase->isConsultable())
+            {
+                $user->deviceID = [];
+                $user->save();
+                $usersCleaned++;
+            }
+        }
+        $this->info($usersCleaned . " deviceID cleaned because users where in an expired case.");
+        if (!App::environment('local'))
+        {
+            WebhookCall::create()
+                ->url(config('utilities.url_rc_registration'))
+                ->payload(['text' => $usersCleaned . " deviceID cleaned because users where in an expired case."])
+                ->useSecret(config('utilities.secret_rc_notifications'))
+                ->dispatch();
+        }
+        return 0;
+    }
+}
