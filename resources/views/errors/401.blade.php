bc45732744b1ae06590a05061549d7cecf61d31d
Alessandro Belli <belli@uni-bremen.de>
2022-06-02 11:48:52 +0200

    better error page
    * now when a users log in in metag analyze and they can't because they're not researcher, they are redirected to the mobile app

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,24 +1,58 @@
-@extends('errors::layout')
+@extends('errors::minimal')
 
 @section('title', 'Error')
 
 
 @section('message')
-    <div class="w-1/3 p-2">
-    <img src="images/undraw_warning_cyit.svg" ì style="max-width: 150px;" alt="Warning!">
-    </div>
-    {{__('This is the backend, you are not authorized to see this content.')}}
-    {{__('You can now login on the mobile app.')}}
-    <a class="font-bold no-underline"
-       style="background: whitesmoke round;border-radius: 10px;padding: 3px;color: lightskyblue;text-decoration: none;display: block;"
-       href="{{ route('logout') }}"
-       onclick="event.preventDefault();
-     document.getElementById('logout-form').submit();">
-        {{ __('Logout') }}
+<div class="w-1/3 p-2">
+    <img src="images/undraw_warning_cyit.svg" style="max-width: 150px;" alt="Warning!">
+</div>
+<p>
+    <a class="btn btn--ios" href="https://apps.apple.com/de/app/metag/id1478391926">
+        <svg class="btn__icon btn__icon--apple" version="1.1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M17.5640259,13.8623047
+        c-0.4133301,0.9155273-0.6115723,1.3251343-1.1437988,2.1346436c-0.7424927,1.1303711-1.7894897,2.5380249-3.086853,2.5500488
+        c-1.1524048,0.0109253-1.4483032-0.749939-3.0129395-0.741333c-1.5640259,0.008606-1.8909302,0.755127-3.0438843,0.7442017
+        c-1.296814-0.0120239-2.2891235-1.2833252-3.0321655-2.4136963c-2.0770874-3.1607666-2.2941895-6.8709106-1.0131836-8.8428955
+        c0.9106445-1.4013062,2.3466187-2.2217407,3.6970215-2.2217407c1.375,0,2.239502,0.7539673,3.3761597,0.7539673
+        c1.1028442,0,1.7749023-0.755127,3.3641357-0.755127c1.201416,0,2.4744263,0.6542969,3.3816528,1.7846069
+        C14.0778809,8.4837646,14.5608521,12.7279663,17.5640259,13.8623047z M12.4625244,3.8076782
+        c0.5775146-0.741333,1.0163574-1.7880859,0.8571167-2.857666c-0.9436035,0.0653076-2.0470581,0.6651611-2.6912842,1.4477539 C10.0437012,3.107605,9.56073,4.1605835,9.7486572,5.1849365C10.7787476,5.2164917,11.8443604,4.6011963,12.4625244,3.8076782z" />
+        </svg>
+        <span class="btn__text">Download on the</span>
+        <span class="btn__storename">App Store</span>
     </a>
+    <a class="btn btn--android"
+        href="http://play.google.com/store/apps/details?id=de.unibremen.metag&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+        <svg class="btn__icon btn__icon--google" version="1.1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" viewBox="0 0 20 20">
+            <path d="M4.942627,18.0508423l7.6660156-4.3273926l-1.6452026-1.8234253L4.942627,18.0508423z M2.1422119,2.1231079
+            C2.0543823,2.281311,2,2.4631958,2,2.664917v15.1259766c0,0.2799683,0.1046143,0.5202026,0.2631226,0.710144l7.6265259-7.7912598
+            L2.1422119,2.1231079z M17.4795532,9.4819336l-2.6724854-1.508606l-2.72229,2.7811279l1.9516602,2.1630249l3.4431152-1.9436035
+            C17.7927856,10.8155518,17.9656372,10.5287476,18,10.2279053C17.9656372,9.927063,17.7927856,9.6402588,17.4795532,9.4819336z
+             M13.3649292,7.1592407L4.1452026,1.954834l6.8656616,7.609314L13.3649292,7.1592407z" />
+        </svg>
+        <span class="btn__text">Get it on</span>
+        <span class="btn__storename">Google Play</span>
+    </a>
+</p>
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
+
+
+
+{{__('This is the backend, you are not authorized to see this content.')}}
+{{__('You can now login on the mobile app.')}}
+<a class="font-bold no-underline"
+    style="background: whitesmoke round;border-radius: 10px;padding: 3px;color: lightskyblue;text-decoration: none;display: block;"
+    href="{{ route('logout') }}" onclick="event.preventDefault();
+     document.getElementById('logout-form').submit();">
+    {{ __('Logout') }}
+</a>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+</form>
 
 @endsection
--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,7 +13,166 @@
 
     <!-- Styles -->
     <style>
-        html, body {
+        .btn {
+            box-sizing: border-box;
+            display: inline-block;
+            text-align: left;
+            white-space: nowrap;
+            text-decoration: none;
+            vertical-align: middle;
+            touch-action: manipulation;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            border: 1px solid #ddd;
+            padding: 4px 8px;
+            margin: 5px auto;
+            border-radius: 4px;
+            color: #fff;
+            fill: #fff;
+            background: #000;
+            line-height: 1em;
+            min-width: 190px;
+            height: 45px;
+            transition: 0.2s ease-out;
+            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+            font-family: $btn-font;
+            font-weight: 500;
+            text-rendering: optimizeLegibility;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            -moz-font-feature-settings: 'liga', 'kern';
+        }
+
+        .btn:hover,
+        .btn:focus {
+            background: #111;
+            color: #fff;
+            fill: #fff;
+            border-color: #fff;
+            transform: scale(1.01) translate3d(0, -1px, 0);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
+
+        .btn:active {
+            outline: 0;
+            background: #353535;
+            transition: none;
+        }
+
+        .btn__icon,
+        .btn__text,
+        .btn__storename {
+            display: inline-block;
+            vertical-align: top;
+        }
+
+        .btn__icon {
+            width: 30px;
+            height: 30px;
+            margin-right: 5px;
+            margin-top: 2px;
+        }
+
+        .btn__icon--amazon {
+            transform: scale(0.85);
+        }
+
+        .btn__text {
+            letter-spacing: 0.08em;
+            margin-top: -0.1em;
+            font-size: 10px;
+        }
+
+        .btn__storename {
+            display: block;
+            margin-left: 38px;
+            margin-top: -17px;
+            font-size: 22px;
+            letter-spacing: -0.03em;
+        }
+
+        .btn--small {
+            padding: 2px 8px;
+            min-width: 118.75px;
+            height: 24px;
+            border-radius: 3px;
+        }
+
+        .btn--small .btn__icon {
+            width: 16px;
+            height: 16px;
+            margin: 1px 2px 0 0;
+        }
+
+        .btn--small .btn__text {
+            display: none;
+        }
+
+        .btn--small .btn__storename {
+            font-size: 12px;
+            display: inline-block;
+            margin: 0;
+            vertical-align: middle;
+        }
+
+        .btn--tiny {
+            padding: 3px;
+            width: 22px;
+            height: 22px;
+            min-width: 0;
+            border-radius: 3px;
+        }
+
+        .btn--tiny .btn__icon {
+            width: 14px;
+            height: 14px;
+            margin: 0;
+        }
+
+        .btn--tiny .btn__text,
+        .btn--tiny .btn__storename {
+            display: none;
+        }
+
+        html,
+        body {
+            background: #f4b350;
+            width: 100%;
+            height: 100%;
+        }
+
+        body {
+            box-sizing: border-box;
+            padding: 0 10%;
+            max-width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            background-image: radial-gradient(ellipse at center, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 80%);
+        }
+
+        .stage>h1 {
+            margin-top: -5%;
+            margin-bottom: 5%;
+            font-family: Avenir, Trebuchet, 'Trebuchet MS', sans-serif;
+            font-size: 7vw;
+            font-weight: 400;
+            color: #c67d0c;
+        }
+
+        @media (min-width: 50em) {
+            .stage>h1 {
+                font-size: 5vw;
+            }
+        }
+
+        html,
+        body {
             background-color: #fff;
             color: #636b6f;
             font-family: 'Nunito', sans-serif;
@@ -48,16 +208,18 @@
         }
     </style>
 </head>
-<body>
-<div class="flex-center position-ref full-height">
-    <div class="code">
-        @yield('code')
-    </div>
 
-    <div class="message" style="padding: 10px;">
-        @yield('message')
-        @yield('url')
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="code">
+            @yield('code')
+        </div>
+
+        <div class="message" style="padding: 10px;">
+            @yield('message')
+            @yield('url')
+        </div>
     </div>
-</div>
 </body>
+
 </html>
