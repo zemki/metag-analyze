fe6e929f4a59deca48c506e8a76250dcd3aab5af
Alessandro Belli <belli@uni-bremen.de>
2022-06-02 13:21:02 +0200

    Better packages + login/register
    * updated login and register to reflect new layout
    * updated package.json
    * new tailwind config file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,112 @@
 @extends('auth.layouts.app')
 
 @section('content')
-    <div class="flex sm:items-start md:items-center md:justify-center h-screen">
-        <div class="bg-white p-4 rounded overflow-hidden shadow-lg  md:w-1/2 lg:w-1/3 sm:w-full">
-            @if(session()->has('message') || !empty($message))
-                <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
-                    <p class="font-bold">{{session()->get('message') ? session()->get('message') : $message}}</p>
-                </div>
-            @endif
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
 
-                <figure class="w-1/4 m-auto max-w-full">
-                    <img src="{{config('utilities.base64logo')}}" alt="Metag Logo">
-                </figure>
-                <div class="text-center">
-                    <h1 class="text-4xl pb-2 m-auto max-w-full font-extrabold">Metag Analyze</h1>
-                    <h4> {{ __('Login') }} </h4>
-                    <div class="py-4 w-full text-center ">
-                        <a class="text-blue-500 hover:text-red-600"
-                           href="{{url('register')}}">{{__("Register to use Metag Analyze")}}</a>
-                    </div>
-                </div>
+<div class="p-4 mx-2 rounded-md bg-yellow-50 md:hidden" role="alert">
+  <div class="flex">
+    <div class="flex-shrink-0">
 
-                <label for="email" class="label">{{ __('E-Mail Address') }}</label>
-                <input id="email" type="email"
-                       class="input {{ $errors->has('email') ? ' bg-red-800 text-white' : '' }}" name="email"
-                       value="{{ old('email') }}" required autofocus>
-
-                <div class="my-2">
-                    <label for="password" class="label">{{ __('Password') }}</label>
-
-                    <input id="password" type="password"
-                           class="input {{ $errors->has('password') ? ' bg-red-800  text-white' : '' }}"
-                           name="password"
-                           required>
-                </div>
-                <a class="text-blue-500 hover:text-red-600 block"
-                   href="{{url('password/reset')}}">{{__("Forgot Password?")}}
-                </a>
-                @if ($errors->has('email'))
-                    <div class="bg-red-700 my-2 pl-2 py-2 text-white font-bold">
-                        {{ $errors->first('email') }}
-                    </div>
-                @endif
-
-                @if ($errors->has('password'))
-                    <div class="bg-red-700 my-2  pl-2 py-2 text-white font-bold">
-                        {{ $errors->first('password') }}
-                    </div>
-                @endif
-                <div class="text-center align-middle">
-
-                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
-                        {{__("Login")}}
-                    </button>
-
-                </div>
-            </form>
-        </div>
+      <svg class="w-5 h-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+        aria-hidden="true">
+        <path fill-rule="evenodd"
+          d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+          clip-rule="evenodd" />
+      </svg>
     </div>
+    <div class="ml-3">
+      <h3 class="text-sm font-medium text-yellow-800">{{__('Attention Needed')}}</h3>
+      <div class="mt-2 text-sm text-yellow-700">
+        <p>{{__('Metag Analyze is intended to be used on a Tablet or Computer')}}</p>
+      </div>
+    </div>
+  </div>
+</div>
+@if(session()->has('message') || !empty($message))
+<div class="p-4 rounded-md bg-red-50">
+  <div class="flex">
+    <div class="flex-shrink-0">
+      <!-- Heroicon name: solid/x-circle -->
+      <svg class="w-5 h-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+        aria-hidden="true">
+        <path fill-rule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+          clip-rule="evenodd" />
+      </svg>
+
+    </div>
+    <div class="ml-3">
+      <div class="mt-2 text-sm text-red-700">
+        {{session()->get('message') ? session()->get('message') : $message}}
+      </div>
+    </div>
+  </div>
+</div>
+@endif
+<div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+  <div class="w-full max-w-md space-y-8">
+    <div>
+      <img class="w-auto h-24 mx-auto" src="{{config('utilities.base64logo')}}" alt="Metag Analyze Logo">
+      <h1 class="mt-6 text-3xl font-extrabold text-center text-gray-900">{{__('Sign in to Metag Analyze')}}</h1>
+      <p class="mt-2 text-sm text-center text-gray-600">
+        Or
+        <a href="{{url('register')}}" class="font-medium text-blue-500 hover:text-blue-700"> {{__('Register to use Metag
+          Analyze')}}
+        </a>
+      </p>
+    </div>
+    <form class="mt-8 space-y-6" action="{{ route('login') }}" method="POST">
+      <input type="hidden" name="remember" value="true">
+      @csrf
+      <div class="-space-y-px rounded-md shadow-sm">
+        <div>
+          <label for="email-address" class="sr-only">{{ __('E-Mail Address') }}</label>
+          <input id="email-address" name="email" type="email" autocomplete="email" required
+            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+            placeholder="{{ __('E-Mail Address') }}">
+        </div>
+        <div>
+          <label for="password" class="sr-only">{{ __('Password') }}</label>
+          <input id="password" name="password" type="password" autocomplete="current-password" required
+            class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+            placeholder="{{ __('Password') }}">
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+
+        <div class="text-sm">
+          <a href="{{url('password/reset')}}" class="font-medium text-blue-600 hover:text-blue-700"> Forgot your
+            password? </a>
+        </div>
+      </div>
+
+      @if ($errors->has('email'))
+      <div class="py-2 pl-2 my-2 font-bold text-white bg-red-700 rounded-none appearance-none rounded-b-md">{{
+        $errors->first('email') }}
+      </div>
+      @endif
+
+      @if ($errors->has('password'))
+      <div class="py-2 pl-2 my-2 font-bold text-white bg-red-700 rounded-none appearance-none rounded-b-md">{{
+        $errors->first('password') }}
+      </div>
+      @endif
+      <div>
+        <button type="submit"
+          class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md group hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+          <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+            <!-- Heroicon name: solid/lock-closed -->
+            <svg class="w-5 h-5 text-blue-500 group-hover:text-blue-400" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd"
+                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                clip-rule="evenodd" />
+            </svg>
+          </span>
+          {{__("Login")}}
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
 @endsection
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 
-    <div class="flex sm:items-start md:items-center md:justify-center h-screen z-10">
-        <div class="bg-white p-4 rounded overflow-hidden shadow-lg  md:w-1/2 lg:w-1/3 sm:w-full">
 
-            <h1 class="text-4xl pb-2 m-auto max-w-full font-extrabold text-center">{{ __('Verify Your Email Address') }}</h1>
+    <div class="flex h-screen sm:items-start md:items-center md:justify-center">
+        <div class="p-4 overflow-hidden bg-white rounded shadow-lg sm:w-full md:w-1/2 lg:w-1/3">
+
+            <h1 class="max-w-full pb-2 m-auto text-4xl font-extrabold text-center">{{ __('Verify Your Email Address') }}</h1>
             <div class="text-justify">
                 {{ __('Before proceeding, please check your email for a verification link.') }}
                 <form id="resend-form" method="POST" action="{{route('verification.resend')}}">
@@ -17,18 +18,27 @@
                                                                  onclick="event.preventDefault();
                         document.getElementById('resend-form').submit();">
                     {{ __('click here to request another') }}
+                </a> <br>
+                <a class="mt-2 text-blue-700" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+
+                    {{ __('Logout') }}
                 </a>.
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
 
 
             @if (session('resent'))
-                <div class="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3"
+                <div class="flex items-center px-4 py-3 text-sm font-bold text-white bg-blue-500"
                      role="alert">
-                    <svg class="fill-current w-4 mb-2 font-medium leading-tight text-xl mr-2"
+                    <svg class="w-4 mb-2 mr-2 text-xl font-medium leading-tight fill-current"
                          xmlns="http://www.w3.org/2000/svg"
                          viewBox="0 0 20 20">
                         <path
-                                d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/>
+                            d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/>
                     </svg>
 
                     <p> {{ __('A fresh verification link has been sent to your email address.') }} </p>
@@ -37,4 +47,6 @@
 
         </div>
     </div>
+
+
 @endsection
