c43b5beaf8042d2b856af13bd02eb0b2891039cb
Alessandro Belli <belli@uni-bremen.de>
2023-11-01 11:27:08 +0100

    Update audioplayer.vue

--- resources/js/components/audioplayer.vue
+++ resources/js/components/audioplayer.vue
@@ -203,7 +203,7 @@ export default {
     // eslint-disable-next-line prefer-destructuring
     this.audio = this.$el.querySelectorAll("audio")[0];
     this.audio.addEventListener("timeupdate", this.update);
-    this.audio.addEventListener("canplaythrough", this.load); // Change this
+    this.audio.addEventListener("loadeddata", this.load); // Change this
     this.audio.addEventListener("pause", () => {
       this.playing = false;
     });
@@ -237,16 +237,18 @@ export default {
       a.click();
     },
     load() {
-      this.loaded = true;
-      this.durationSeconds = parseInt(this.audio.duration, 10);
-      console.log('Loaded', this.durationSeconds); // Add this
-      return this.playing === this.autoPlay;
+      if (this.audio.duration && !isNaN(this.audio.duration)) {
+        this.loaded = true;
+        this.durationSeconds = parseInt(this.audio.duration, 10);
+        console.log('Loaded', this.durationSeconds);
+        return this.playing === this.autoPlay;
+      }
     },
 
 
     update() {
       this.currentSeconds = parseInt(this.audio.currentTime, 10);
-      console.log('Update', this.currentSeconds); // Add this
+      console.log('Update', this.currentSeconds);
     },
 
     confirmDeleteFile() {
