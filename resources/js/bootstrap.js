223ff1c4db19e437e50b41ca86185cd0ba88f9fc
Alessandro Belli <belli@uni-bremen.de>
2024-12-11 13:12:09 +0300

    correct files

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,10 +7,10 @@ window._ = require("lodash");
  */
 
 try {
-  //window.Popper = require('popper.js').default;
+
   window.$ = window.jQuery = require("jquery");
 
-  //require("bootstrap");
+
 } catch (e) {}
 
 /**
@@ -38,20 +38,3 @@ if (token) {
     "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
   );
 }
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
-// });
--- resources/js/components.js
+++ resources/js/components.js
@@ -1,44 +1,36 @@
 import Vue from "vue";
 import Gravatar from "vue-gravatar";
+import EditProject from "./components/editproject.vue";
+import ProjectInvites from "./components/projectsInvites.vue";
 
-Vue.component("edit-project", require("./components/editproject.vue").default);
-Vue.component(
-  "consult-entries",
-  require("./components/consultentries.vue").default
-);
-Vue.component(
-  "project-invites",
-  require("./components/projectsInvites.vue").default
-);
-Vue.component("user-table", require("./components/usertable.vue").default);
-Vue.component("graph", require("./components/graph.vue").default);
-Vue.component(
-  "users-in-cases",
-  require("./components/users-in-cases.vue").default
-);
+import Graph from "./components/graph.vue";
 
-Vue.component(
-  "medtaggraph",
-  require("./components/groupedentries.vue").default
-);
+import MedTagGraph from "./components/groupedentries.vue";
+import NotificationCenter from "./components/notificationcenter.vue";
+import AudioPlayer from "./components/audioplayer.vue";
+import ProjectsList from "./components/projects-list.vue";
+import Toast from "./components/toast.vue";
+import ModalEditEntry from "./components/modal_edit_entry.vue";
+import SelectedCase from "./components/selected-case.vue";
+import createproject from "./components/createproject.vue";
+import Modal from "./components/global/modal.vue";
+import Snackbar from "./components/global/snackbar.vue";
+import CustomDialogue from "./components/global/CustomDialogue.vue";
+import Caseslist from "./components/caseslist.vue";
 
-Vue.component(
-  "notification-center",
-  require("./components/notificationcenter.vue").default
-);
-
-Vue.component("audio-player", require("./components/audioplayer.vue").default);
+Vue.component("edit-project", EditProject);
+Vue.component("project-invites", ProjectInvites);
+Vue.component("graph", Graph);
+Vue.component("medtaggraph", MedTagGraph);
+Vue.component("notification-center", NotificationCenter);
+Vue.component("audio-player", AudioPlayer);
 Vue.component("v-gravatar", Gravatar);
-Vue.component(
-  "projects-list",
-  require("./components/projects-list.vue").default
-);
-Vue.component("toast", require("./components/toast.vue").default);
-Vue.component(
-  "modal_edit_entry",
-  require("./components/modal_edit_entry.vue").default
-);
-Vue.component(
-  "selected-case",
-  require("./components/selected-case.vue").default
-);
+Vue.component("projects-list", ProjectsList);
+Vue.component("toast", Toast);
+Vue.component("modal_edit_entry", ModalEditEntry);
+Vue.component("selected-case", SelectedCase);
+Vue.component("create-project", createproject);
+Vue.component("modal", Modal);
+Vue.component("snackbar", Snackbar);
+Vue.component("custom-dialogue", CustomDialogue);
+Vue.component("cases-list", Caseslist);
